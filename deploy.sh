088f0b02781693e06806d5fbff09c69431bba718
Jiffy <jiffyzhang@qq.com>
2021-03-16 13:51:40 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd blog/.vuepress/dist
+cd public
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:xunlu1998/xunlu1998.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xunlu1998/blog.git master:xunlu
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
