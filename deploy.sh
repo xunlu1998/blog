f6db54598f11d97a3cdb1ea03691d332cd7563c5
Jiffy <jiffyzhang@qq.com>
2021-03-16 13:14:58 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
-# 确保脚本抛出遇到的错误
-set -e
+
 
 # 生成静态文件
-yarn build
+npm run build
 
 # 进入生成的文件夹，这里是默认的路径，可以自定义
 cd .vuepress/dist
+
+# 确保脚本抛出遇到的错误
+set -e
 
 # 如果是发布到自定义域名
 # echo 'www.isunbeam.cn' > CNAME
@@ -14,11 +16,11 @@ git init
 git add -A
 git commit -m 'deploy'
 
+set -e
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:<BranchName>
 git push -f git@github.com:xunlu1998/blog.git master:xunlu
 
-
-cd -
 
 # 最后发布的时候执行 bash deploy.sh
